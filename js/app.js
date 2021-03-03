a3c95170b25cdbe92b09088bac3ca6c381f64c19
furatmalkawi29 <malkawifurat29@gmail.com>
2021-03-03 16:48:21 +0200

    edits on cart.js

--- js/app.js
+++ js/app.js
@@ -1,37 +1,35 @@
 'use strict';
 
 // Cart constructor.
-const Cart = function(items) {
+const Cart = function (items) {
   // this.items is an array of CartItem instances.
   this.items = items;
 };
 
-Cart.prototype.addItem = function(product, quantity) {
+Cart.prototype.addItem = function (product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
-  const newItem = new CartItem(product,quantity);
-  this.items.push(newItem);
+  this.items.push(new CartItem(product, quantity));
 };
 
-Cart.prototype.saveToLocalStorage = function() {
+Cart.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
   localStorage.setItem('cart', JSON.stringify(this.items));
 };
 
-Cart.prototype.removeItem = function(item) {
+Cart.prototype.removeItem = function (item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
-
-  this.items.splice(item, 1);
-
+  const index = this.items.indexOf(item);
+  this.items.splice(index, 1);
 };
 
-const CartItem = function(product, quantity) {
+let CartItem = function (product, quantity) {
   this.product = product;
   this.quantity = quantity;
 };
 
 // Product contructor.
-const Product = function(filePath, name) {
+const Product = function (filePath, name) {
   this.filePath = filePath;
   this.name = name;
   Product.allProducts.push(this);
--- js/cart.js
+++ js/cart.js
@@ -2,13 +2,16 @@
 'use strict';
 
 // Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
-const table = document.getElementById('cart');
-table.addEventListener('click', removeItemFromCart);
+const table = document.getElementById( 'cart' );
+table.addEventListener( 'click', removeItemFromCart );
 let cart;
 
+
+// gets item array from --> local storage | or set cartTems as empty array
 function loadCart() {
-  const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
-  cart = new Cart(cartItems);
+  const cartItems = JSON.parse( localStorage.getItem( 'cart' ) ) || [];
+  cart = new Cart( cartItems );
+  console.log ()
 }
 
 // Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
@@ -19,21 +22,41 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  document.getElementsByTagName( 'tbody' ).innerHtml = '';
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
-
+  let tbodyElement = document.getElementsByTagName( 'tbody' );
   // TODO: Iterate over the items in the cart
+  for ( let i = 0 ; i < Cart.items.length ; i++ )
+  {
   // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+    let trElement = document.createElement( 'tr' );
 
+    // TODO: Create a TD for the delete link, quantity,  and the item
+    let tdDeleteElement = document.createElement( 'td' );
+    let tdItemElement = document.createElement( 'td' );
+    let tdQutElement = document.createElement( 'td' );
+
+    // tdDeleteElement.textContent = 'X';
+    // tdItemElement.textContent = Cart.items[i].product;
+    // tdQutElement.textContent = Cart.items[i].quantity;
+
+    // TODO: Add the TR to the TBODY and each of the TD's to the TR
+    tdDeleteElement.appendChild( trElement );
+    tdItemElement.appendChild( trElement );
+    tdQutElement.appendChild( trElement );
+
+    tbodyElement.appendChild( trElement );
+
+  }
 }
 
-function removeItemFromCart(event) {
+function removeItemFromCart( event ) {
 
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
